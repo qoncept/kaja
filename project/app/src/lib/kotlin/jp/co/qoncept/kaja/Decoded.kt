8058f2227edfc8e1d7e4cf3c1c10a15a7ff8d85e
Nisho Matsushita <robonich@gmail.com>
2016-04-12 23:12:47 +0900

    Implement Decoded class

--- project/app/src/lib/kotlin/jp/co/qoncept/kaja/Decoded.kt
+++ project/app/src/lib/kotlin/jp/co/qoncept/kaja/Decoded.kt
@@ -2,37 +2,104 @@ package jp.co.qoncept.kaja
 
 abstract sealed class Decoded<T> {
     class Success<T>(value: T): Decoded<T>() {
-        // TODO
+        override val value: T? = value
+
+        override val exception: DecodeException? = null
+
+        override fun <U> map(transform: (T) -> U): Decoded<U> {
+            return pure(transform(value!!))
+        }
+
+        override fun <U> flatMap(transform: (T) -> Decoded<U>): Decoded<U>{
+            return transform(value!!)
+        }
+
+        override fun <U> apply(transform: Decoded<(T) -> U>): Decoded<U> {
+            return when(transform) {
+                is Success -> this.map { transform.value!!(it) }
+                is Failure -> Failure(transform.exception!!)
+            }
+        }
+
+        override fun ifMissingKey(alternative: T): Decoded<T> {
+            return this
+        }
+
+        override fun or(alternative: Decoded<T>): Decoded<T> {
+            return this
+        }
+
+        override fun or(alternative: T): T {
+            return value!!
+        }
     }
 
     class Failure<T>(exception: DecodeException): Decoded<T>() {
-        // TODO
-    }
+        override val value: T? = null
 
+        override val exception: DecodeException? = exception
+
+        override fun <U> map(transform: (T) -> U): Decoded<U> {
+            return Failure(exception!!)
+        }
+
+        override fun <U> flatMap(transform: (T) -> Decoded<U>): Decoded<U> {
+            return Failure(exception!!)
+        }
+
+        override fun <U> apply(transform: Decoded<(T) -> U>): Decoded<U> {
+            return when(transform) {
+                is Success -> Failure(exception!!)
+                is Failure -> Failure(transform.exception!!)
+            }
+        }
+
+        override fun ifMissingKey(alternative: T): Decoded<T> {
+            return when(exception!!) {
+                is MissingKeyException -> Success(alternative)
+                else -> this
+            }
+        }
+
+        override fun or(alternative: Decoded<T>): Decoded<T> {
+            return alternative
+        }
+
+        override fun or(alternative: T): T {
+            return alternative
+        }
+    }
     abstract val value: T?
+
     abstract val exception: DecodeException?
 
-    abstract fun <T> or(alternative: T): T
+    abstract fun or(alternative: T): T
 
-    abstract fun <T> or(alternative: Decoded<T>): Decoded<T>
+    abstract fun or(alternative: Decoded<T>): Decoded<T>
 
-    abstract fun <T> ifMissingKey(alternative: T): Decoded<T>
+    abstract fun ifMissingKey(alternative: T): Decoded<T>
 
-    abstract fun <T, U> map(transform: (T) -> U): Decoded<U>
+    abstract fun <U> map(transform: (T) -> U): Decoded<U>
 
-    abstract fun <T, U> flatMap(transform: (T) -> Decoded<U>): Decoded<U>
+    abstract fun <U> flatMap(transform: (T) -> Decoded<U>): Decoded<U>
 
-    abstract fun <T, U> apply(transform: Decoded<(T) -> U>): Decoded<U>
+    abstract fun <U> apply(transform: Decoded<(T) -> U>): Decoded<U>
 }
 
 fun <T> Decoded<Decoded<T>>.flatten(): Decoded<T> {
-    // TODO
+    return when(this) {
+        is Decoded.Success -> value!!
+        is Decoded.Failure -> Decoded.Failure(exception!!)
+    }
 }
 
 fun <T, U> Decoded<(T) -> U>.ap(value: T): Decoded<U> {
-    // TODO
+    return when(this) {
+        is Decoded.Success -> this.map { it(value) }
+        is Decoded.Failure -> Decoded.Failure(exception!!)
+    }
 }
 
 fun <T> pure(value: T): Decoded<T> {
-    return Decoded.success(value)
+    return Decoded.Success(value)
 }
