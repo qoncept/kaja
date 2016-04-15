ca3de94180b87f5d355cd1c1e2b515c12478e180
S-Shimotori <490.frostybirds@gmail.com>
2016-04-15 16:02:20 +0900

    Fix sequence #3

--- project/app/src/lib/kotlin/jp/co/qoncept/kaja/sequence.kt
+++ project/app/src/lib/kotlin/jp/co/qoncept/kaja/sequence.kt
@@ -9,35 +9,24 @@ fun <T> sequence(xs: List<Decoded<T>>): Decoded<List<T>> {
                 accum.add(x.value!!)
                 accum
             }
-            is Decoded.Failure -> return Decoded.Failure(x.exception!!)
+            else -> return Decoded.Failure(x.exception!!)
         }
     })
 }
 
 fun <Key, Value> sequence(xs: Map<Key, Decoded<Value>>): Decoded<Map<Key, Value>> {
-    /*
     return pure(xs.fold(HashMap<Key, Value>()) { accum, entry ->
         when (entry.value) {
             is Decoded.Success -> {
                 accum.put(entry.key, entry.value.value!!)
                 accum
             }
-            is Decoded.Failure -> return Decoded.Failure(entry.value.exception!!)
+            else -> return Decoded.Failure(entry.value.exception!!)
         }
-    })*/
-    var accum = HashMap<Key, Value>()
-
-    for ((key, x) in xs) {
-        when (x) {
-            is Decoded.Success -> accum.put(key, x.value!!)
-            else -> return Decoded.Failure(x.exception!!)
-        }
-    }
-
-    return pure(accum)
+    })
 }
 
-fun <Key, Value, R> Map<Key, Value>.fold(initial: R, operation: (R, Map.Entry<Key, Value>) -> R): R {
+inline fun <Key, Value, R> Map<Key, Value>.fold(initial: R, operation: (R, Map.Entry<Key, Value>) -> R): R {
     var result = initial
     for (entry in this) {
         result = operation(initial, entry)
