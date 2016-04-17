73c75970afa9f8f0c337a148b717ce3003624557
Nisho Matsushita <robonich@gmail.com>
2016-04-17 17:46:22 +0900

    Add src/test/kotlin to test.java.srcDirs in build.gradle

---
+++ project/app/src/test/kotlin/jp/co/qoncept/kaja/DecodedTest.kt
@@ -0,0 +1,68 @@
+package jp.co.qoncept.kaja
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by nisho on 16/04/17.
+ */
+class DecodedTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun testGetValue() {
+
+    }
+
+    @Test
+    fun testGetException() {
+
+    }
+
+    @Test
+    fun testOr() {
+
+    }
+
+    @Test
+    fun testOr1() {
+
+    }
+
+    @Test
+    fun testIfMissingKey() {
+
+    }
+
+    @Test
+    fun testNullIfMissingKey() {
+
+    }
+
+    @Test
+    fun testMap() {
+
+    }
+
+    @Test
+    fun testFlatMap() {
+
+    }
+
+    @Test
+    fun testApply() {
+
+    }
+}
