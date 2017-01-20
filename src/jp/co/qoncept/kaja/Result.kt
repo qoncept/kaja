1bd3cdda9851df76d22ecad096e63e8a9a1ccbbe
Yuta Koshizawa <koher@koherent.org>
2017-01-20 19:00:35 +0900

    Add `Result`'s property extensions

--- src/jp/co/qoncept/kaja/Result.kt
+++ src/jp/co/qoncept/kaja/Result.kt
@@ -9,3 +9,21 @@ operator fun Result<Json, JsonException>.get(key: String): Result<Json, JsonExce
 
 val Result<Json, JsonException>.boolean: Result<Boolean, JsonException>
     get() = flatMap { it.boolean }
+
+val Result<Json, JsonException>.int: Result<Int, JsonException>
+    get() = flatMap { it.int }
+
+val Result<Json, JsonException>.long: Result<Long, JsonException>
+    get() = flatMap { it.long }
+
+val Result<Json, JsonException>.double: Result<Double, JsonException>
+    get() = flatMap { it. double}
+
+val Result<Json, JsonException>.string: Result<String, JsonException>
+    get() = flatMap { it. string}
+
+val Result<Json, JsonException>.list: Result<List<Json>, JsonException>
+    get() = flatMap { it.list }
+
+val Result<Json, JsonException>.map: Result<Map<String, Json>, JsonException>
+    get() = flatMap { it.map }
